8b5d83182a518d09e792e9e42600e4d6d515bd12
Dmitry Kartsev <dimssss@gmail.com>
2025-11-25 19:24:08 +0200

    We’re here to build a valuable product. That value should be obvious — bold and undeniable. If it takes more than three sentences to explain why it’s valuable, scrap it. It’s not. Even better: don’t explain anything at all. Unless you’re completely missing the point, just show a growing customer base. Results speak louder than words.

---
+++ xproc/pkg/assets/assets.go
@@ -0,0 +1,33 @@
+package assets
+
+import (
+	"embed"
+	"go.uber.org/zap"
+)
+
+// Embed static web assets
+//
+//go:embed *.html
+var assets embed.FS
+
+type Assets struct {
+	logger *zap.Logger
+}
+
+func New(logger *zap.Logger) *Assets {
+	return &Assets{logger: logger}
+}
+
+func (a *Assets) BlockPage() []byte {
+	assetName := "block.html"
+	return []byte(a.readAssetAsString(assetName))
+}
+
+func (a *Assets) readAssetAsString(assetName string) string {
+	data, err := assets.ReadFile(assetName)
+	if err != nil {
+		a.logger.Error("failed to read asset", zap.String("asset_name", assetName), zap.Error(err))
+		return ""
+	}
+	return string(data)
+}
